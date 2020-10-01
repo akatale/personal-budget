96eaa5163da851d1a63f90de4f3a6c4b1b97e8f3
Ashwin Katale <akatale@uncc.edu>
2020-10-01 17:51:06 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,29 +4,15 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+var fetch = require('./server.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    // res.json(budget);
+    res.json(fetch);
 });
 
 app.listen(port, () => {
