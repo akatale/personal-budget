dd95398d1e1ea7b4b7cf0353611f157b7dfdf9fd
Ashwin Katale <akatale@uncc.edu>
2020-10-04 16:27:39 -0400

    CORS added

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+// app.use('/', express.static('public'));
+app.use(cors());
 
 var fetch = require('./server.json');
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.get('/budget', (req, res) => {
     // res.json(budget);
@@ -16,5 +18,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}', port);
+    console.log('API served at http://localhost:', +port);
 });
